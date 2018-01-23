0e539059db333e63587cc561529460125a6e7f1a
Noble_ <1055806142@qq.com>
2018-01-23 15:38:24 +0800

    删除无意义文件

--- 菜鸡入门篇/Hailstone序列.cpp
+++ 菜鸡入门篇/Hailstone序列.cpp
@@ -1,34 +1,30 @@
- #include <iostream>
-using namespace std;
+#include <iostream>
+#include <vector>
 
-void recursion(int num)
-{
-	if(buff<=1)
-	{
-		v.push_back(1);
-		return;
-	}
-	else if(buff%2==0)
-	{
-		v.push_back(num);
-		recursion(num/2);
-		return;
-	}
-	else 
-	{
-		v.push_back(num);
-		recursion(3*num+1);
-	}
+using namespace std;
+vector<int> v;
+
+void recursion(int num) {
+    if (num <= 1) {
+        v.push_back(1);
+        return;
+    } else if (num % 2 == 0) {
+        v.push_back(num);
+        recursion(num / 2);
+        return;
+    } else {
+        v.push_back(num);
+        recursion(3 * num + 1);
+        return;
+    }
 }
-int main()
-{
-	vector<int> v(0);
-	int buff;
-	cin>>buff;
-	recursion(buff);
-	for(int i =0;i<v.size(),i++)
-	{
-		cout<<v[i];
-	}
-	return 0;
+
+int main() {
+    int buff;
+    cin >> buff;
+    recursion(buff);
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i]<<endl;
+    }
+    return 0;
 }
